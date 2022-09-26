867ad56c67dfa6f044b86f377af6fa2eebfbe2d2
WijayaTimothyLarry <wijayatimothylarry@gmail.com>
2022-09-26 12:31:59 +0800

    fix styling

--- src/Pages/Events/index.js
+++ src/Pages/Events/index.js
@@ -2,10 +2,21 @@ import React from "react";
 import EventsBox from "./EventsBox/index";
 
 import "./style.scss";
+import { Header1 } from "Styles/Typography";
 const EventsPage = () => {
   return (
     <div className="events-page">
+      <div className="events-page__title">
+        <Header1>Events</Header1>
+      </div>
       <div className="events-page__events-wrapper">
+        <EventsBox />
+        <EventsBox />
+        <EventsBox />
+        <EventsBox />
+        <EventsBox />
+        <EventsBox />
+        <EventsBox />
         <EventsBox />
         <EventsBox />
         <EventsBox />

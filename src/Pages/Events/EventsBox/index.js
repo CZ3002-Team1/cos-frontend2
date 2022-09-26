d958476456843dad5648b0076de0d673cc1416c5
WijayaTimothyLarry <wijayatimothylarry@gmail.com>
2022-09-26 13:03:22 +0800

    integrate get API

--- src/Pages/Events/EventsBox/index.js
+++ src/Pages/Events/EventsBox/index.js
@@ -1,23 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { Body2, Header2 } from "Styles/Typography/index";
 
-import Modal from "antd/lib/modal/Modal";
-
 import "./style.scss";
 import EventDetailModal from "./../EventDetailModal/index";
 
-const EventsBox = () => {
+const EventsBox = ({ data }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const data = {
-    Name: "Having Fun",
-    Description: "string",
-    StartDate: "2022-09-26",
-    EndDate: "2022-09-26",
-    Time: "Time",
-    PhotoUrl:
-      "https://costeam1.s3.amazonaws.com/fileupload/scanskill-1664157327335-4.png",
-  };
 
+  console.log(typeof data.EndDate);
+  console.log(new Date(data.EndDate));
   const showModal = () => {
     setIsOpen(true);
   };
@@ -39,7 +30,8 @@ const EventsBox = () => {
         <div className="events-box__bottom">
           <Header2>{data.Name}</Header2>
           <Body2>
-            {data.StartDate} - {data.EndDate}
+            {new Date(data.StartDate).toDateString()} -
+            {new Date(data.EndDate).toDateString()}
           </Body2>
         </div>
       </div>
--- src/Pages/Events/index.js
+++ src/Pages/Events/index.js
@@ -1,27 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 import EventsBox from "./EventsBox/index";
+import { Header1 } from "Styles/Typography";
 
 import "./style.scss";
-import { Header1 } from "Styles/Typography";
+
 const EventsPage = () => {
+  const [data, setData] = useState([]);
+  const [displayData, setDisplayData] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      await axios
+        .get("http://localhost:5000/api/event")
+        .then((res) => {
+          console.log(res.data.data);
+          setDisplayData(res.data.data);
+          setData(res.data.data);
+        })
+        .catch((err) => {
+          alert(err.message);
+          // setDisplayData(dummyData);
+          // setData(dummyData);
+        });
+    };
+    getData();
+  }, []);
+
   return (
     <div className="events-page">
       <div className="events-page__title">
         <Header1>Events</Header1>
       </div>
       <div className="events-page__events-wrapper">
-        <EventsBox />
-        <EventsBox />
-        <EventsBox />
-        <EventsBox />
-        <EventsBox />
-        <EventsBox />
-        <EventsBox />
-        <EventsBox />
-        <EventsBox />
-        <EventsBox />
-        <EventsBox />
-        <EventsBox />
+        {displayData ? (
+          displayData.map((d) => <EventsBox data={d} key={d._id} />)
+        ) : (
+          <div />
+        )}
       </div>
     </div>
   );

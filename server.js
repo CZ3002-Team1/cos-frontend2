34d802a309132a32cd9b16c40e227c6102534ae7
WijayaTimothyLarry <wijayatimothylarry@gmail.com>
2022-10-06 22:04:23 +0800

    'init'

--- server.js
+++ server.js
@@ -4,8 +4,15 @@ var express = require("express");
 var app = express();
 
 app.use(express.static(path.join(__dirname, "dist")));
-app.set("port", process.env.PORT || 8080);
+app.set("port", process.env.PORT || 9000);
 
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "dist/index.html"), function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
 var server = app.listen(app.get("port"), function () {
   console.log("listening on port ", server.address().port);
 });
---
+++ webpack.prod.config.js
@@ -0,0 +1,98 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  mode: "production",
+  entry: [
+    "regenerator-runtime/runtime.js",
+    path.resolve(__dirname, "src", "index.js"),
+  ],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    assetModuleFilename: "images/[hash][ext][query]",
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        include: path.resolve(__dirname, "src"),
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                [
+                  "@babel/preset-env",
+                  {
+                    targets: "defaults",
+                  },
+                ],
+                "@babel/preset-react",
+              ],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.less$/i,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "less-loader",
+            options: {
+              lessOptions: {
+                strictMath: true,
+                javascriptEnabled: true,
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.png/,
+        type: "asset/resource",
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        use: ["@svgr/webpack"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx", ".json"],
+    alias: {
+      Src: path.resolve(__dirname, "./src"),
+      App: path.resolve(__dirname, "./src/App"),
+      Commons: path.resolve(__dirname, "./src/Commons"),
+      Pages: path.resolve(__dirname, "./src/Pages"),
+      PintuShop: path.resolve(__dirname, "./src/Pages/PintuShop"),
+      Styles: path.resolve(__dirname, "./src/Styles"),
+      Fonts: path.resolve(__dirname, "./src/Styles/Fonts"),
+      GlobalStyles: path.resolve(__dirname, "./src/Styles/Global"),
+    },
+  },
+};

971860866168beb4f7ef6152d7b692412b93d8e7
WijayaTimothyLarry <wijayatimothylarry@gmail.com>
2022-10-21 21:56:55 +0800

    added date filter

--- src/Pages/Events/index.js
+++ src/Pages/Events/index.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import moment from "moment";
 
 import { createNewEvent, deleteEvent, getEvents } from "./EventReducer";
 
+import { DatePicker } from "antd";
 import EventsBox from "./EventsBox/index";
-import { Header1, Header2 } from "Styles/Typography";
+import { Header1, Header2, Body1 } from "Styles/Typography";
 import CustomButton from "Commons/CustomButton";
 import NewEventForm from "./NewEventForm";
 
@@ -49,12 +51,26 @@ const EventsPage = () => {
     dispatch(deleteEvent(_id));
   };
 
+  const handleDateFilter = (date) => {
+    console.log(date);
+    if (date === null) {
+      setDisplayData(eventInfo.eventList);
+    } else {
+      const filteredEvent = eventInfo.eventList.filter((event) => {
+        const startDate = moment(event.StartDate, "YYYY-MM-DD");
+        const endDate = moment(event.endDate, "YYYY-MM-DD");
+        return date >= startDate && date <= endDate;
+      });
+      setDisplayData(filteredEvent);
+    }
+  };
+
   return (
     <div className="events-page">
       <div className="events-page__title">
         <Header1>Welcome Back {userInfo.Name}</Header1>
         <div>
-          <Header1>Events</Header1>
+          <Header1>Events </Header1>
 
           {userInfo.IsAdmin && (
             <div className="events-page__title__button">
@@ -69,6 +85,10 @@ const EventsPage = () => {
               </CustomButton>
             </div>
           )}
+          <div>
+            <Body1>Filter by month: </Body1>
+            <DatePicker onChange={handleDateFilter} picker="month" />
+          </div>
           {eventInfo.status === "fulfilled" &&
             eventInfo.eventList.length === 0 && (
               <div>
